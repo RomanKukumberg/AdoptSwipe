0ff9e8359080f8b636871f9faa3d9570ac789a77
Roman <r_kukumberg@utb.cz>
2025-12-14 23:33:51 +0100

    animal data

---
+++ app/src/main/java/com/example/adoptswipe/data/model/Animal.kt
@@ -0,0 +1,10 @@
+package com.example.adoptswipe.data.model
+
+data class Animal(
+    val id: Int,
+    val name: String,
+    val age: String,
+    val breed: String,
+    val imageUrl: String,
+    val description: String
+)
